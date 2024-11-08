d0c41edb63810cc4072c9ecebe3f04660f65919f
archMqq <clouztoyou@mail.ru>
2024-11-08 19:56:54 +0300

    Добавьте файлы проекта.

---
+++ FeedbackService.ServiceResult/ServiceErrors.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FeedbackService.ServiceResult
+{
+    public class ServiceErrors
+    {
+        public string Error {  get; set; }
+        [DefaultValue(null)]
+        public string[]? Fields { get; set; }
+    }
+}
---
+++ FeedbackService.ServiceResult/ServiceResult.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FeedbackService.ServiceResult
+{
+    public class ServiceResult<T>
+    {
+        [DefaultValue(false)]
+        public bool BadRequest { get; set; }
+        [DefaultValue(false)]
+        public bool NotFound { get; set; }
+        [DefaultValue(false)]
+        public bool InternalServerError { get; set; }
+        public T Result { get; set; }
+        public ServiceErrors Errors { get; set; }
+    }
+    }
